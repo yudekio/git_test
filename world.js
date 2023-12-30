3cd1bd095071c29e17d1664170f9a6b52db7c9a1
yudekio <145836654+yudekio@users.noreply.github.com>
2023-12-30 16:43:36 +0100

    Created 2 pages: world creator and world

--- world.js
+++ world.js
@@ -17,3 +17,27 @@ function init() {
 init();
 
 console.log(worldData.name);
+
+function createCanvas() {
+  let cellCounter = 0;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("div");
+    row.style = "display: flex; height: calc(62% / 6); width: 62%";
+    for (let j = 0; j < 6; j++) {
+      let cell = document.createElement("div");
+      cell.style = `width: 100%;
+      height: 100%;
+                background-color: #0baa60;
+                border: 2px solid rgb(189, 189, 189);
+                box-sizing: border-box;`;
+      cellCounter++;
+      cell.setAttribute("index", cellCounter);
+
+      row.appendChild(cell);
+    }
+    document.querySelector("#game").appendChild(row);
+  }
+}
+
+createCanvas();
+console.log(document.querySelector('[index="35"]'));
